e4ed3684e5362666f265640238209737d8a7e9bb
posiputt <i@posiputt.de>
2020-12-26 00:45:59 +0100

    minor changes in index.php

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 ?>
   <div id="everything">
     <div id="content">
-      <div id="navtop"><?php turn_page($_GET["page"], 1); echo "\t".$_GET["page"]."\t"; turn_page($_GET["page"], -1);?></div>
-      <div id="page"><?php show_page($_GET["page"]);?></div>
-      <div id="social">
-	<?php show_flattr_btn();?>
-      </div>
+    <div id="navtop">
+    <?php
+	turn_page($_GET["page"], 1, "yes");
+	echo "\t".$_GET["page"]."\t";
+	turn_page($_GET["page"], -1, "yes");
+    ?></div>
+      <div id="page"><?php show_image($_GET["page"]);?></div>
     </div>
    </div>
   </body>
